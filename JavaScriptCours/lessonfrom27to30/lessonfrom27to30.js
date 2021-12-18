a948cabe83c07c1b2b927186922feb652495682d
3thmantwt <64757028+3thmantwt@users.noreply.github.com>
2021-12-18 18:13:45 +0300

    تم الحل

--- JavaScriptCours/lessonfrom27to30/lessonfrom27to30.js
+++ JavaScriptCours/lessonfrom27to30/lessonfrom27to30.js
@@ -17,6 +17,6 @@ let letterZ = "z";
 let letterE = "e";
 let letterO = "O";
 
-console.log(); // True
-console.log(); // True
-console.log(); // True
+console.log(letterZ.startsWith(word[2])); // True
+console.log(word.startsWith(letterE.toUpperCase())); // True
+console.log(word.endsWith(letterO.toLowerCase())); // True
