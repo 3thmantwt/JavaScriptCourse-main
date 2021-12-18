77aedb14d66c7ff1ad76c496b877dda4ca9f43b3
3thmantwt <64757028+3thmantwt@users.noreply.github.com>
2021-12-18 13:56:49 +0300

    حل تكليفات lesson_from_27_to_30

--- JavaScriptCours/lessonfrom27to30/lessonfrom27to30.js
+++ JavaScriptCours/lessonfrom27to30/lessonfrom27to30.js
@@ -1,2 +1,10 @@
-document.write('<h1> Helllo lessonfrom27to30</h1>');
-console.log('dddddddd');
+
+//                    [التكليف 01  ]
+
+let userName = "Elzero";
+console.log(userName.charAt(0).toLowerCase()); // e
+console.log(userName[0].toLowerCase()); // e
+console.log(userName.slice(0,1).toLowerCase()); // e
+console.log(userName.substring(0,1).toLowerCase()); // e
+console.log(); // e
+console.log(); // eee
