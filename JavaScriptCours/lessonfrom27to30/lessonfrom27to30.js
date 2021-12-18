655209ae7d0f7fd8e1db80318540329bc80c713a
3thmantwt <64757028+3thmantwt@users.noreply.github.com>
2021-12-18 14:16:22 +0300

    تحديث الحل

--- JavaScriptCours/lessonfrom27to30/lessonfrom27to30.js
+++ JavaScriptCours/lessonfrom27to30/lessonfrom27to30.js
@@ -6,5 +6,17 @@ console.log(userName.charAt(0).toLowerCase()); // e
 console.log(userName[0].toLowerCase()); // e
 console.log(userName.slice(0,1).toLowerCase()); // e
 console.log(userName.substring(0,1).toLowerCase()); // e
-console.log(); // e
-console.log(); // eee
+console.log(userName.split("",1).toString().toLowerCase()); // e
+console.log(userName.slice(-6,-5).toLowerCase().repeat(3)); // eee
+
+//                    [التكليف 02  ]
+
+
+let word = "Elzero";
+let letterZ = "z";
+let letterE = "e";
+let letterO = "O";
+
+console.log(); // True
+console.log(); // True
+console.log(); // True
