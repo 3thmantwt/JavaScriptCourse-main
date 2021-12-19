201d58eb84aec2b019725f778d39214b2898aee3
3thmantwt <64757028+3thmantwt@users.noreply.github.com>
2021-12-19 22:32:56 +0300

    add New Folder

---
+++ JavaScriptCours/lessonfrom31to39/lessonfrom38to39/If And Switch Toggle/If And Switch Toggle.js
@@ -0,0 +1,20 @@
+/*
+
+== == == == == == ==    If And Switch Toggle    == == == == == == == == 
+
+*/
+
+console.log("If And Switch Toggle");
+
+let job = "Manager";
+let salary = 0;
+
+if (job === "Manager") {
+  salary = 8000;
+} else if (job === "IT" || job === "Support") {
+  salary = 6000;
+} else if (job === "Developer" || job === "Designer") {
+  salary = 7000;
+} else {
+  salary = 4000;
+}
