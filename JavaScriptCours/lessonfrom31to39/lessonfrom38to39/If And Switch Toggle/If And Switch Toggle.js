5eabda84d11e893f031760c03f8a85f4e01c8703
3thmantwt <64757028+3thmantwt@users.noreply.github.com>
2021-12-20 20:29:45 +0300

    تحديث

--- JavaScriptCours/lessonfrom31to39/lessonfrom38to39/If And Switch Toggle/If And Switch Toggle.js
+++ JavaScriptCours/lessonfrom31to39/lessonfrom38to39/If And Switch Toggle/If And Switch Toggle.js
@@ -4,7 +4,10 @@
 
 */
 
-console.log("If And Switch Toggle");
+/*
+    Switch Challenge
+ */
+console.log("Switch Challenge");
 
 let job = "Manager";
 let salary = 0;
@@ -18,3 +21,79 @@ if (job === "Manager") {
 } else {
   salary = 4000;
 }
+
+switch (job) {
+    case "Manager":
+        salary = 8000;
+        console.log(`Salary The [Manager]is : ${salary} SAR`);
+        break;
+    case "IT":
+    case "Support":
+        salary = 6000;
+        console.log(`Salary The [IT] and [Support]is : ${salary} SAR`);
+    case "Developer":
+    case "Designer" :
+        salary = 7000;
+        console.log(`Salary The [Developer] and [Designer]is : ${salary} SAR`);
+    default:
+        salary = 4000;
+        console.log(`Salary for auther job is : ${salary} SAR`);
+        break;
+}
+
+console.log("If Challenge")
+
+/*
+If Challenge
+ */
+
+let holidays = 0;
+let money = 0;
+
+switch (holidays) {
+    case 0:
+      money = 5000;
+      console.log(`My Money is ${money}`);
+      break;
+    case 1:
+    case 2:
+      money = 3000;
+      console.log(`My Money is ${money}`);
+      break;
+    case 3:
+      money = 2000;
+      console.log(`My Money is ${money}`);
+      break;
+    case 4:
+      money = 1000;
+      console.log(`My Money is ${money}`);
+      break;
+    case 5:
+      money = 0;
+      console.log(`My Money is ${money}`);
+      break;
+    default:
+      money = 0;
+      console.log(`My Money is ${money}`);
+  }
+
+if (holidays === 0) {
+    money = 5000;
+    console.log(`My Money is ${money}`);
+} else if (holidays === 1 || holidays === 2){
+    money = 3000;
+    console.log(`My Money is ${money}`);
+} else if (holidays === 3){
+    money = 2000;
+    console.log(`My Money is ${money}`);
+} else if (holidays === 4){
+    money = 1000;
+    console.log(`My Money is ${money}`);
+} else if (holidays === 5){
+    money = 0;
+    console.log(`My Money is ${money}`);
+} else {
+    money = 0;
+    console.log(`My Money is ${money}`);
+}
+
