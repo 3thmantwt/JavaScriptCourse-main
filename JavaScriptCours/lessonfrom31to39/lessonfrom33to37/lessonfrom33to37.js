05a5cbcad19daf8e6bdca9b15b7f0bc5b6d3d6f3
3thmantwt <64757028+3thmantwt@users.noreply.github.com>
2021-12-20 22:00:08 +0300

    حل lessonfrom33to37

--- JavaScriptCours/lessonfrom31to39/lessonfrom33to37/lessonfrom33to37.js
+++ JavaScriptCours/lessonfrom31to39/lessonfrom33to37/lessonfrom33to37.js
@@ -1,22 +1,71 @@
-// Test Case 1
-let num = 9; // "009"
+// /*
+//                             [  التكليف 01  ]
+// */
 
-// Test Case 2
-let num = 20; // "020"
+// // // Test Case 1
+// // let num = 9; // "009"
 
-// Test Case 3
-let num = 110; // "110"
+// // // Test Case 2
+// // let num = 20; // "020"
 
-if (num < 10) {
+// // // // Test Case 3
+// let num = 120; // "110"
 
-    console.log("009");
+// if (num < 10) {
 
-}else if(num > 10 && num < 100){
+//     console.log(`00${num}`);
 
-    console.log("020");
+// }else if(num > 10 && num < 100){
 
-}else if(num >= 100){
+//     console.log(`0${num}`);
 
-    console.log("110");
-}
+// }else if (num >= 100){
+
+//     console.log(num);
+// }
+
+// /*
+//                             [  التكليف 02  ]
+// */
+// // [==] The Same Value
+// // [!==] The Same Value As But Not The Same Type
+
+// let num1 = 9;
+// let str = "9";
+// let str2 = "20";
+
+// if (num1 == str) { 
+//     console.log(`${num1} Is The Same Value As ${str}`);
+// } if (num1 !== str){ 
+//     console.log(`${num1} Is The Same Value As ${str} But Not The Same Type`);
+// } if (num1 !== str2) {
+//     console.log(`${num1} Is Not The Same Value Or The Same Type As ${str}`);
+// } if (typeof str === typeof str2) {
+//     console.log(`${num1} Is The Same Type As ${str} But Not The Same Value`);
+// }
+
+// // Output
+// "{num1} Is The Same Value As {str}" // Done
+// "{num1} Is The Same Value As {str} But Not The Same Type" // Done
+// "{num1} Is Not The Same Value Or The Same Type As {str2}" // Done
+// "{str} Is The Same Type As {str2} But Not The Same Value" // Done
+
+/*
+                            [  التكليف 03  ]
+*/
+
+let num1 = 10;
+let num2 = 30;
+let num3 = "30";
+(num3 > num1 && typeof num3 !== typeof num2 ) ? 
+console.log("30 Is Larger Than 10 And Type string Not The Same Type As number"): 
+(num3 > num1 && num3 !== num2) ? 
+console.log("30 Is Larger Than 10 And Value Is The Same As 30 And Type string Not The Same Type As number"):
+(num3 != num1 && num3 !== num2) ? 
+console.log(`${num3} Value And Type Is Not The Same As ${num1} And Type Is Not The Same As ${num2}`):
+
+// Needed Output
+"30 Is Larger Than 10 And Type string Not The Same Type As number"
+"30 Is Larger Than 10 And Value Is The Same As 30 And Type string Not The Same Type As number"
+"{num3} Value And Type Is Not The Same As {num1} And Type Is Not The Same As {num2}"
 
