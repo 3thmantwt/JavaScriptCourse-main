b5b5511b0a2a92e39d6ebaa96dfba722209cc1fc
3thmantwt <64757028+3thmantwt@users.noreply.github.com>
2021-12-20 22:55:54 +0300

    Done

--- JavaScriptCours/lessonfrom31to39/lessonfrom33to37/lessonfrom33to37.js
+++ JavaScriptCours/lessonfrom31to39/lessonfrom33to37/lessonfrom33to37.js
@@ -54,18 +54,92 @@
                             [  التكليف 03  ]
 */
 
-let num1 = 10;
-let num2 = 30;
-let num3 = "30";
-(num3 > num1 && typeof num3 !== typeof num2 ) ? 
-console.log("30 Is Larger Than 10 And Type string Not The Same Type As number"): 
-(num3 > num1 && num3 !== num2) ? 
-console.log("30 Is Larger Than 10 And Value Is The Same As 30 And Type string Not The Same Type As number"):
-(num3 != num1 && num3 !== num2) ? 
-console.log(`${num3} Value And Type Is Not The Same As ${num1} And Type Is Not The Same As ${num2}`):
+// let num1 = 10;
+// let num2 = 30;
+// let num3 = "30";
+// (num3 > num1 && typeof num3 !== typeof num2 ) ? 
+// console.log("30 Is Larger Than 10 And Type string Not The Same Type As number"): 
+// (num3 > num1 && num3 !== num2) ? 
+// console.log("30 Is Larger Than 10 And Value Is The Same As 30 And Type string Not The Same Type As number"):
+// (num3 != num1 && num3 !== num2) ? 
+// console.log(`${num3} Value And Type Is Not The Same As ${num1} And Type Is Not The Same As ${num2}`):
 
-// Needed Output
-"30 Is Larger Than 10 And Type string Not The Same Type As number"
-"30 Is Larger Than 10 And Value Is The Same As 30 And Type string Not The Same Type As number"
-"{num3} Value And Type Is Not The Same As {num1} And Type Is Not The Same As {num2}"
+// // Needed Output
+// "30 Is Larger Than 10 And Type string Not The Same Type As number"
+// "30 Is Larger Than 10 And Value Is The Same As 30 And Type string Not The Same Type As number"
+// "{num3} Value And Type Is Not The Same As {num1} And Type Is Not The Same As {num2}"
+
+/*
+                            [  التكليف 04  ]
+*/
+
+// Edit What You Want Here
+
+let num1 = 10;
+let num2  = 1;
+let num3 = 10;
+let num4 = 40;
+
+/*
+  Do Not Edit Below This Line
+  Needed Output
+  True 7 Times
+*/
+
+// Condition 1
+
+if (num1 > num2) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 2
+
+if (num1 > num2 && num1 < num4) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 3
+
+if (num1 > num2 && num1 === num3) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 4
+
+if ((num1 + num2) < num4) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 5
+
+if ((num1 + num3) < num4) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 6
+
+if ((num1 + num2 + num3) < num4) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 7
+
+if (num4 - (num1 + num3) + num2 === 21) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
 
