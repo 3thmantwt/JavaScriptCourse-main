40440e2791aa6ee908ad615e4bedd43f7173864c
3thmantwt <64757028+3thmantwt@users.noreply.github.com>
2021-12-21 22:05:57 +0300

    Done Array Methods Challenge

---
+++ JavaScriptCours/lessonfrom40to47/Array Methods Challenge/Array Methods Challenge.js
@@ -0,0 +1,33 @@
+
+
+let zero = 0;
+let counter = 3;
+let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
+
+//          ======  [  01  ]  ======
+
+// console.log(my.slice(zero , counter + true).reverse());   // Done
+
+console.log(my.slice(zero , counter + true).reverse()); // ["Osama", "Elham", "Mazero", "Ahmed"];
+
+
+//          ======  [  02  ]  ======
+
+// console.log(my.slice(false - counter - true - true , false - counter ).reverse()) // Done
+
+console.log(my.slice(false - counter - true - true , false - counter ).reverse()); // ["Elham", "Mazero"]
+
+
+//          ======  [  03  ]  ======
+
+console.log(my.push("Elzero"))
+
+my.reverse()
+console.log(my[zero]); // "Elzero"
+
+
+//          ======  [  04  ]  ======
+
+// console.log(my[zero].slice(counter + true,counter+true+true)+my[zero].slice(counter+counter-true).toUpperCase())
+
+console.log(my[zero].slice(counter + true,counter+true+true)+my[zero].slice(counter+counter-true).toUpperCase()); // "rO"
