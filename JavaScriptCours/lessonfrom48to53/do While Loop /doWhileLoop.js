5a00e51d25a24504112ec3afeefebdb785ffa206
3thmantwt <64757028+3thmantwt@users.noreply.github.com>
2021-12-27 20:40:57 +0300

    do while chal

--- JavaScriptCours/lessonfrom48to53/do While Loop /doWhileLoop.js
+++ JavaScriptCours/lessonfrom48to53/do While Loop /doWhileLoop.js
@@ -1,6 +1,7 @@
 let myAdmins = ["Ahmed", "Osama", "Sayed", "Stop", "Samera"];
-let myEmployees = ["Amgad", "Samah", "Ameer", "Omar", "Othman", "Amany", "Samia"];
-
+let myEmployees = ["Amgad", "Samah", "Ameer", "Omar", "Othman", "Amany", "Samia", "Ali"];
+let a = [];
+let count = 0;
 // start
 
 document.write(`<div>We Have X Admins</div>`)
@@ -12,19 +13,39 @@ if (myAdmins[3] === "Stop") {
 
 document.write('<hr/>') // hr
 
-// 1 
-
-// let i = 0;
-// do {
-//     i = i + 1
-//     document.write(`<div>The Admin For Team  [${i}] Is ${myAdmins[0]}</div>`)
-//     i++
-// } while (false);
-// do {
-//     document.write(`<div>We Have ${i} Admins</div>`)
-//     i++
-// } while (false);
 for (let i = 0; i < myAdmins.length; i++) {
+    // Main loop
 
-    document.write(`<div>The Admin For Team  [${i + 1}] Is ${myAdmins[i]}</div>`)
+    // nested Loop
+    document.write(`<div>The Admin For Team  [${i + 1}] Is ${myAdmins[i]} <h3>Team Members : </h3><p> </P></div>`)
+    for (let x = 0; x < myEmployees.length; x++) {
+
+        // === A ===
+        if (myAdmins[i].startsWith("A")) {
+            if (myEmployees[x].startsWith("A")) {
+
+                document.write(`<p> - ${myEmployees[x]} </p>`)
+            }
+            // === O ===
+        }
+        if (myAdmins[i].startsWith("O")) {
+            if (myEmployees[x].startsWith("O")) {
+
+                document.write(`<p> - ${myEmployees[x]} </p>`)
+            }
+
+
+            // === S ===
+        }
+        if (myAdmins[i].startsWith("S")) {
+            if (myEmployees[x].startsWith("S")) {
+
+                document.write(`<p> - ${myEmployees[x]} </p>`)
+            }
+
+
+
+        }
+    }
+    document.write(`<hr/>`)
 }
